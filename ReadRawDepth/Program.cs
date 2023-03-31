6055fdf0e0620a790e867a17782119bf6618d01e
李兴钢 <lixinggang@bytedance.com>
2023-03-31 20:28:36 +0800

    准备读取16bit数值

--- ReadRawDepth/Program.cs
+++ ReadRawDepth/Program.cs
@@ -8,7 +8,7 @@ namespace ReadRawDepth
     {
         [Option('f', "file", Required = true, HelpText = "输入要读取的文件")]
         [NotNull]
-        public string File { get; set; }
+        public string File { get; set; } = string.Empty;
         [Option('s', "scale", Required = false, Default = 1, HelpText = "如果图片不明显,可以输入这个变的明显一点")]
         public int Scale { get; set; }
         [Option('g', "gamma", Required = false, Default = false, HelpText = "true = 对图片反向gamma矫正,同时忽略scale属性")]
@@ -18,7 +18,7 @@ namespace ReadRawDepth
     {
         static void Main(string[] args)
         {
-            string path = null;
+            string path = string.Empty;
             int scale = 1;
             bool gamma = false;
             Parser.Default.ParseArguments<Options>(args)
@@ -32,40 +32,9 @@ namespace ReadRawDepth
             {
                 throw new NullReferenceException("no path to read");
             }
-            var raw = File.ReadAllBytes(path);
-            var fullPath = Path.GetFullPath(path);
-            var fileName = Path.GetFileNameWithoutExtension(fullPath);
-            var directory = Path.GetDirectoryName(fullPath);
-            //注意,我们这个贴图是没有padding的
-            using (Mat mat = new Mat(528, 528, MatType.CV_32FC1, raw, 0))
+            using (var d = new ReadRawDepth(path, 528, 528, 1.0f, true))
             {
-                //Mat showing = new Mat();
-                //Cv2.ConvertScaleAbs(mat, showing, 25500, 0);
-                mat.MinMaxIdx(out var minIdx, out var maxIdx);
-                Console.WriteLine($"min = {minIdx}, max = {maxIdx}");
-                var saved = Path.Combine(directory, $"{fileName}.png");
-                Console.WriteLine(saved);
-                mat.SaveImage(saved);
-
-                //using var showing = mat.Normalize(0, 255, NormTypes.Hamming2);
-                Mat showing = mat;
-                if (scale != 1.0f)
-                {
-                    showing = mat * (float)scale;
-                }
-                else if (gamma)
-                {
-                    for (int y = 0; y < showing.Rows; y++)
-                    {
-                        for (int x = 0; x < showing.Cols; x++)
-                        {
-                            ref var e = ref showing.At<float>(x, y);
-                            e = MathF.Pow(e, 1f / 2.2f);
-                        }
-                    }
-                }
-                Cv2.ImShow("show", showing);
-                Cv2.WaitKey();
+                d.Show();
             }
         }
     }
---
+++ ReadRawDepth/ReadRawDepth.cs
@@ -0,0 +1,86 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadRawDepth
+{
+    internal class ReadRawDepth : IDisposable
+    {
+        public ReadRawDepth(string path, int width, int height, float scale, bool gamma)
+        {
+            var raw = File.ReadAllBytes(path);
+            _mat = new Mat(width, height, MatType.CV_32FC1, raw, 0);
+            _path = path;
+            if (scale != 1.0f)
+            {
+                _mat = _mat * scale;
+            }
+            else if (gamma)
+            {
+                var showing = _mat;
+                for (int y = 0; y < showing.Rows; y++)
+                {
+                    for (int x = 0; x < showing.Cols; x++)
+                    {
+                        ref var e = ref showing.At<float>(x, y);
+                        e = MathF.Pow(e, 1f / 2.2f);
+                    }
+                }
+            }
+        }
+        public void Save()
+        {
+            var fullPath = Path.GetFullPath(_path);
+            var fileName = Path.GetFileNameWithoutExtension(fullPath);
+            var directory = Path.GetDirectoryName(fullPath);
+            var mat = _mat;
+            mat.MinMaxIdx(out var minIdx, out var maxIdx);
+            Console.WriteLine($"min = {minIdx}, max = {maxIdx}");
+            var saved = Path.Combine(directory, $"{fileName}.png");
+            Console.WriteLine(saved);
+            mat.SaveImage(saved);
+        }
+        public void Show()
+        {
+            var showing = _mat;
+            Cv2.ImShow("show", showing);
+            Cv2.WaitKey();
+        }
+        private Mat _mat;
+        private string _path;
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)
+                    _mat.Dispose();
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                // TODO: 将大型字段设置为 null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        // ~ReadRawDepth()
+        // {
+        //     // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
